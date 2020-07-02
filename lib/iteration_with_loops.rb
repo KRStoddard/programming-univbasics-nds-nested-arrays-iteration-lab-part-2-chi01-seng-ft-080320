af12de5869d8e529f2703d00ca5da371bb246183
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-02 16:53:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,8 @@
 def find_min_in_nested_arrays(src)
   index_count = 0 
-  
+  while index_count < src.length do
+    element_index = 0
+    low = 0 
+    while element_index > src[index_count].length 
+    
 end
